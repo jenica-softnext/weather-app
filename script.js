fc56e42b12dc16dfced42c7dc5aaee6c4db240d1
jen <Jenica@pilipinas.softnext.co.jp>
2024-05-09 11:11:19 +0800

    update js and html

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-// 10:48
+const weatherContainer = document.querySelector(".weather-container");
+const weatherInfo = document.querySelector(".weather-info");
+const temperature = document.querySelector(".temperature");
+const weatherDesc = document.querySelector(".description");
+const weatherIcon = document.getElementById("icon");
+
+const getWeather = function () {
+  const apiKey = "ced1fbc6df5983dfde3fdf06e6d4ecbf";
+  const city = document.getElementById("city").value;
+
+  if (!city) {
+    alert("Please enter a city");
+  }
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+  `)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.cod == "404") {
+        alert(data.message);
+      } else {
+        return showTemp(data);
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+const showTemp = function (data) {
+  const temp = data.main.temp;
+  const place = data.name;
+  const desc = data.weather[0].description;
+  const icon = data.weather[0].icon;
+  temperature.innerHTML = `The weather in ${place} is <span>${temp}°C</span>`;
+  weatherInfo.style.height = "360px";
+  weatherDesc.innerHTML = desc;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+
+  if (temp >= 35) {
+    document.body.classList.add("hot");
+    document.body.classList.remove("warm");
+    document.body.classList.remove("cold");
+  } else if (temp > 20 && temp < 35) {
+    document.body.classList.remove("hot");
+    document.body.classList.add("warm");
+    document.body.classList.remove("cold");
+  } else if (temp <= 20) {
+    document.body.classList.remove("hot");
+    document.body.classList.remove("warm");
+    document.body.classList.add("cold");
+  }
+};
